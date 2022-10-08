a3e513faa18870d002098e2c2b01f488d4555451
Guilherme Bandeira <107276182+Asphyan@users.noreply.github.com>
2022-10-07 23:55:19 -0300

    Update Q-2.cpp

--- LLPR_EXERCÍCIOS/LISTA-1/QUESTÕES/Q-2.cpp
+++ LLPR_EXERCÍCIOS/LISTA-1/QUESTÕES/Q-2.cpp
@@ -7,7 +7,7 @@ int main () {
 	
 	float n1, n2, n3, media;
 	
-	printf("Calculadora de Média\n\n");
+	printf("Calculadora de MÃ©dia\n\n");
 	
 	printf("Digite Sua Primeira Nota:");
 	scanf("%f", &n1);
@@ -18,12 +18,12 @@ int main () {
 
 	media= n1+n2+n3/3;
 	
-	printf("O Valor calculado da medida entre os números %f, %f e %f:\n", n1, n2, n3);
+	printf("O Valor calculado da medida entre os nÃºmeros %f, %f e %f:\n", n1, n2, n3);
 	
-	printf("Sua Média Foi: %f\n", media);
+	printf("Sua MÃ©dia Foi: %f\n", media);
 	
 	return 0;
 	
-	//Quando tiramos os parenteses a divisão é priorizada, para depois a soma ser efetuada.
+		//Quando tiramos os parenteses a divisÃ£o Ã© priorizada, para depois a soma ser efetuada.
 }
 
